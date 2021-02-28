4285347ff8fdd7b316e50deac5f6b39a87203562
Дмитрий Сысков <sdimas55@mail.ru>
2021-02-28 17:15:01 +0300

    adding playfer

--- playfer.py
+++ playfer.py
@@ -1,10 +1,10 @@
 def playfer():
     alpha = [
-            ["a","b","c","d","e"],
-            ["f","g","h","i","k"],
-            ["l","m","n","o","p"],
-            ["q","r","s","t","u"],
-            ["v","w","x","y","z"]
+            ["a","w","c","y","e"],
+            ["k","t","h","r","f"],
+            ["p","m","n","o","l"],
+            ["q","i","s","g","u"],
+            ["v","b","x","d","z"]
             ]
     print('Зашифровать - 1, расшифровать - 2')
     choose = int(input())
@@ -24,17 +24,62 @@ def compile(alpha):
 
     if len(s) % 2 != 0:
         s.append('x')
-
+    answer = ''
     for i in range(1, len(s),2):
-        answer=crypt(s[i-1],s[i])
+        answer+=crypt(s[i-1],s[i],alpha)
+    print(answer)
 
-    return 0
-
-def crypt(a,b):
-    s=a+b
+def crypt(a,b,alpha):
+    x1=0
+    y1=0
+    x2=0
+    y2=0
+    for i in range(5):
+        for j in range(5):
+            if alpha[i][j]==a:
+                x1=i
+                y1=j
+            if alpha[i][j]==b:
+                x2=i
+                y2=j
+    #x - строка y - столбец
+    if x1==x2:
+        s=caseone(x1,y1,x2,y2,alpha)
+    elif y1==y2:
+        s=casetwo(x1,y1,x2,y2,alpha)
+    else:
+        s=casethree(x1,y1,x2,y2,alpha)
     return s
 
+def caseone(x1,y1,x2,y2,alpha):
+    if y1<4:
+        y1+=1
+    else:
+        y1=0
 
+    if y2<4:
+        y2+=1
+    else:
+        y2=0
+    s = alpha[x1][y1] + alpha[x2][y2]
+    return s
+
+def casetwo(x1,y1,x2,y2,alpha):
+    if x1<4:
+        x1+=1
+    else:
+        x1=0
+
+    if x2<4:
+        x2+=1
+    else:
+        x2=0
+    s=alpha[x1][y1] + alpha[x2][y2]
+    return s
+
+def casethree(x1,y1,x2,y2,alpha):
+    s=alpha[x1][y2]+alpha[x2][y1]
+    return s
 
 
 def decompile():
